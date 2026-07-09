b865f631f3c293561dcb0a5e38193e6c997f9726
Ahmed Khilji <61839237+ahmyk9@users.noreply.github.com>
2026-07-09 18:59:55 +1000

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let map = new Map()
+
+        for (let num of nums) map.set(num, (map.get(num) || 0) + 1)
+        
+        let sorted = [...map.entries()].sort((a,b) => (b[1] - a[1]))
+
+        let ans = sorted.slice(0,k).map(entry => entry[0])
+
+
+        return ans
+
+
+    }
+}
