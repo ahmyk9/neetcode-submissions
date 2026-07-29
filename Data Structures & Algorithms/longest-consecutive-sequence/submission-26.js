dbc3af8e60d56908e55f50978bc744431b34f043
Ahmed Khilji <61839237+ahmyk9@users.noreply.github.com>
2026-07-29 16:38:11 +1000

    Add: longest-consecutive-sequence - submission-26

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-26.js
@@ -0,0 +1,27 @@
+class Solution {
+
+    longestConsecutive(nums) {
+
+      let map = new Map()
+
+      let longest = 0
+
+      for(let n of nums){
+        map.set(n)
+      }
+      
+      for(let n of nums){
+        if(!map.has(n - 1)){
+          let length = 1
+          while(map.has(n + length)){
+            length = length + 1
+          }
+          longest = Math.max(longest, length)
+        }
+      
+      }
+
+      return longest
+
+    }
+}
