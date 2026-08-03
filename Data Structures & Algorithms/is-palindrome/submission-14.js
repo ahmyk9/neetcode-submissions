5ee5779683fc4279f8b00496a89ae6fa4d057eb3
Ahmed Khilji <61839237+ahmyk9@users.noreply.github.com>
2026-08-03 18:05:35 +1000

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let str = s.toLowerCase().replace(/[^a-z0-9]/g,"")
+
+        let l = 0
+        let r = str.length - 1
+
+        while(l < r){
+            if(str[l] !== str[r]){
+                return false
+            }else{
+                r--
+                l++
+            }
+        }
+
+        return true
+
+
+    }
+}
