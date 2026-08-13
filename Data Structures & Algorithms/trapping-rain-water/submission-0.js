f23f4f52c4e05dd7924ad6a9c3061da59c299329
Ahmed Khilji <61839237+ahmyk9@users.noreply.github.com>
2026-08-13 15:10:49 +1000

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(heights) {
+
+        let forwardMax = []
+        let heightMax = 0
+        for(let i = 0; i < heights.length; i++){
+            if(heights[i] > heightMax){
+                heightMax = heights[i]
+            } 
+            forwardMax.push(heightMax)
+        }
+
+        let backwardMax = []
+        let heightMax2 = heights[heights.length - 1]
+        for(let i = heights.length - 1; i >= 0; i--){
+            if(heights[i] > heightMax2){
+                heightMax2 = heights[i]
+            } 
+            backwardMax.push(heightMax2)
+        }
+        backwardMax.reverse()
+
+        let sum = 0
+        for(let i = 0; i < heights.length; i++){
+            let ans = (Math.min(forwardMax[i], backwardMax[i])) - heights[i]
+            sum = ans + sum
+        }
+        
+        return sum
+
+        
+    }
+}
