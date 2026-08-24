1207f25de5c2957f2131808f4a63071c55ce0ea4
Ahmed Khilji <61839237+ahmyk9@users.noreply.github.com>
2026-08-24 17:57:54 +1000

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+
+        //o(n^2)
+        // for (let i = 0; i < nums.length; i++){
+        //     for (let j = 0; j < nums.length; j++){
+        //         if (nums[i] + nums[j] === target) return [i,j]
+        //     }
+        // }
+
+        //o(n)
+        let map = new Map()
+
+        
+        for (let i = 0; i < nums.length; i++){
+            let ans = target - nums[i]
+            if(map.has(ans) && i !== map.get(ans) ){
+                return [i, map.get(ans)]
+            }
+            map.set(nums[i], i)
+        }
+
+
+        return []
+
+
+
+
+    }
+}
